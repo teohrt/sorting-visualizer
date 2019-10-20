d5fc259f3993d90068ec649167a8824b1297ff0a
Trace <teohrt18@gmail.com>
2019-10-20 16:30:32 -0700

    Create-React-App Skeleton

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import SortingVisualizer from './SortingVisualizer/SortingVisualizer'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SortingVisualizer/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SortingVisualizer() {
+    return (
+      <div>
+        <p> Project Skeleton! </p>
+      </div>
+    );
+  }
+  
+  export default SortingVisualizer;
+  
